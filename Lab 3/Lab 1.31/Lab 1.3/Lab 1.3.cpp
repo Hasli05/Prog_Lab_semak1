76cb300c814182f063c7c347617d315f1d76d2b7
Max Kurko <84683721+Hasli05@users.noreply.github.com>
2022-10-24 22:02:18 +0300

    Add files via upload

---
+++ Lab 3/Lab 1.31/Lab 1.3/Lab 1.3.cpp
@@ -0,0 +1,46 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <iostream>
+#include <Windows.h>
+#include <locale.h>
+#include <math.h> 
+
+
+double perimetr(double firstSide, double secondSide)
+{
+	return firstSide * secondSide;
+}
+
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	printf("Введіть сторони першого прямокутника в форматі а/б: ");
+	double a1, b1;
+	scanf("%lf/%lf", &a1, &b1);
+
+	printf("Введіть сторони другого прямокутника в форматі а/б: ");
+	double a2, b2;
+	scanf("%lf/%lf", &a2, &b2);
+
+	printf("Введіть сторони третього прямокутника в форматі а/б: ");
+	double a3, b3;
+	scanf("%lf/%lf", &a3, &b3);
+
+	double firstRectP = perimetr(a1, b1);
+	double secondRectP = perimetr(a2, b2);
+	double thirthRectP = perimetr(a3, b3);
+
+	if (firstRectP > secondRectP and firstRectP > thirthRectP)
+	{
+		printf("Площя першого прямокутника найбільна і рівна %lf", firstRectP);
+	}
+	else if (secondRectP > thirthRectP) {
+		printf("Площя другого прямокутника найбільна і рівна %lf", secondRectP);
+	}
+	else {
+		printf("Площя третього прямокутника найбільна і рівна %lf", thirthRectP);
+	}
+}
+
---
+++ Lab 3/Lab 1.31/Lab 1.32/Lab 1.32.cpp
@@ -0,0 +1,45 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <iostream>
+#include <Windows.h>
+#include <locale.h>
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+
+    printf("Номер дня тижня: ");
+    int dayOfWeek;
+    scanf("%d", &dayOfWeek);
+
+
+	switch (dayOfWeek)
+	{
+	case 1:
+		printf("Понеділок (Monday)");
+		break;
+	case 2:
+		printf("Вівторок (Tuesday)");
+		break;
+	case 3:
+		printf("Середа (Wednesday)");
+		break;
+	case 4:
+		printf("Четвер (Thursday)");
+		break;
+	case 5:
+		printf("Пятниця (Friday)");
+		break;
+	case 6:
+		printf("Субота (Saturday)");
+		break;
+	case 7:
+		printf("Неділя (Sunday)");
+		break;
+	default:
+		printf("Дня тижня з таким номером не існує!");
+		break;
+	}
+}
+
