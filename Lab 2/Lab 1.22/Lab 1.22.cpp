e9a87721accc97638f6c147954dba0fe52dd71e4
Max Kurko <84683721+Hasli05@users.noreply.github.com>
2022-10-24 22:01:24 +0300

    Add files via upload

---
+++ Lab 2/Lab 1.22/Lab 1.22.cpp
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <locale.h>
+#include <Windows.h>
+#include <Math.h>
+
+
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	printf("Введіть х: ");
+	double x;
+	scanf("%lf", &x);
+
+	double y = 8.4 + (x + 2.4) * log10(x) - cos(x);
+
+	printf("функція 8.4 + (%lf + 2.4) * log10(%lf) - cos(%lf) = %lf", x, x, x, y);
+
+}
